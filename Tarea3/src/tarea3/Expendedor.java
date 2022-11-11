8f0160395f05bd57ea22380a1b1b8415a02c3a0b
Cerch4 <cefranco2021@inf.udec.cl>
2022-11-11 19:17:13 -0300

    Inicio de modificaciones a la clase expendedor, cambie tipo de retorno del metodo CompraBebida a void y agrege el metodo getBebida

--- Tarea3/src/tarea3/Expendedor.java
+++ Tarea3/src/tarea3/Expendedor.java
@@ -5,8 +5,15 @@ public class Expendedor {
     private DepositoBebida cocacola = new DepositoBebida();
     private DepositoBebida sprite = new DepositoBebida();
     private DepositoBebida fanta = new DepositoBebida();
-    public Expendedor(int nbebidas, int precio){
-        this.precio = precio;
+    private Bebida depositoEsp = null;
+    private depositoMoneda Despecial = new depositoMoneda();
+    private int  x;
+    private int y;
+    public Expendedor(){
+        x = 300;
+        y = 200;
+        this.precio = 800;
+        int nbebidas = 30;
         for (int i = 0; i < nbebidas; i++) {
             CocaCola auxC = new CocaCola (i);
             cocacola.addBebida(auxC);
@@ -16,7 +23,7 @@ public class Expendedor {
             fanta.addBebida(auxF);
         }
     }    
-    public Bebida comprarBebida(Moneda m, int n) throws NoHayBebidaException, PagoIncorrectoException, PagoInsuficienteException{
+    public void comprarBebida(Moneda m, int n) throws NoHayBebidaException, PagoIncorrectoException, PagoInsuficienteException{
         int money = m.getValor();
         if (m == null) {
             throw new PagoIncorrectoException("Pago no valido");
@@ -24,13 +31,14 @@ public class Expendedor {
         if (m.getValor()<precio) {
             throw new PagoInsuficienteException("El Monto de Pago no es suficiente para realizar la transaccion");
         }
-        Bebida out = null;
+        
         switch(n){
             case 1: 
-                out = cocacola.getBebida();
-                if (out == null) {
+                depositoEsp = cocacola.getBebida();
+                if (depositoEsp == null) {
                     throw new NoHayBebidaException("Actualmente no hay mas Cocacola, lo sentimos");
                 }
+                Despecial.addMoneda(m);
                 money = money - precio;
                 while(money>0){
                     DVuelto.addMoneda(new Moneda100());
@@ -38,10 +46,11 @@ public class Expendedor {
                 }
                 break;
             case 2: 
-                out = sprite.getBebida();
-                if (out == null) {
+                depositoEsp = sprite.getBebida();
+                if (depositoEsp == null) {
                     throw new NoHayBebidaException("Actualmente no hay mas Sprite, lo sentimos");
                 }
+                Despecial.addMoneda(m);
                 money = money - precio;
                 while(money>0){
                     DVuelto.addMoneda(new Moneda100());
@@ -49,10 +58,11 @@ public class Expendedor {
                 }
                 break;
             case 3: 
-                out = fanta.getBebida();
+                depositoEsp = fanta.getBebida();
                 if (out == null) {
                     throw new NoHayBebidaException("Actualmente no hay mas Fanta, lo sentimos");
                 }
+                Despecial.addMoneda(m);
                 money = money - precio;
                 while(money>0){
                     DVuelto.addMoneda(new Moneda100());
@@ -64,8 +74,8 @@ public class Expendedor {
                     throw new NoHayBebidaException("El numero ingresado no posee una bebida asociada");
                 }
         }
-        return out;
     }
+    
     public Moneda getVuelto(){
         if (DVuelto.check()>0) {
             return DVuelto.getMoneda();
@@ -76,4 +86,12 @@ public class Expendedor {
     public int getPrice(){
         return precio;
     }
+    
+    public Bebida getBebida(){
+        Bebida aux = depositoEsp;
+        depositoEsp = null;
+        return aux;
+    }
+    
+    
 }
